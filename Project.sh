e3debf80f5134f137872261b987cccb85727a3b5
Drunkenguy <107703518+Drunkenguy@users.noreply.github.com>
2023-04-23 13:49:00 +0200

    Update Project.sh

--- Project.sh
+++ Project.sh
@@ -37,48 +37,46 @@ else
                         
                         
 
-        # Check which option the user selected and execute the appropriate action
-        if [ $? -if [ $? -eq 0 ]; then
-        # Extract settings from input string
-            IFS=':' read -r fonts colors placeholder <<< "$settings"
-            if [ "$fonts" = "TRUE" ]; then
-    # Show a dialog to select a font and change the font in the yad window
-    selected_font=$(yad --font \
-        --title="Select a Font" \
-        --fontname="Monospace 12")
-    yad --form --title="Settings" \
-        --text="Choose an option:" \
-        --field="Change Fonts:CHK" \
-        --field="Change Colors:CHK" \
-        --field="Placeholder:CHK" \
-        --font="$selected_font" \
-        --button="OK:0" --button="Cancel:1"
-      
-      
-      
-                # Show a color selection dialog and change the colors in the yad window
-                selected_colors=$(yad --color-selection \
-                    --title="Select Colors")
-                font_color=$(echo "$selected_colors" | awk -F':' '{print $1}')
-                window_color=$(echo "$selected_colors" | awk -F':' '{print $2}')
-                background_color=$(echo "$selected_colors" | awk -F':' '{print $3}')
-                yad --form --title="Settings" \
-                    --text="Choose an option:" \
-                    --field="Change Fonts:CHK" \
-                    --field="Change Colors:CHK" \
-                    --field="Placeholder:CHK" \
-                    --fontcolor="$font_color" \
-                    --backcolor="$background_color" \
-                    --forecolor
-                    ="$window_color" \
-                    --button="OK:0" --button="Cancel:1"
-elif [ "$placeholder" = "TRUE" ]; then
-# Placeholder for future feature
-yad --title="Settings"
---text="This feature is not yet available."
-else
-# Show an error message if no option is selected
-yad --title="Settings"
+                    # Check which option the user selected and execute the appropriate action
+                    if [ $? -eq 0 ]; then
+                        # Extract settings from input string
+                        IFS=':' read -r fonts colors placeholder <<< "$settings"
+                        if [ "$fonts" = "TRUE" ]; then
+                            # Show a dialog to select a font and change the font in the yad window
+                            selected_font=$(yad --font \
+                                --title="Select a Font" \
+                                --fontname="Monospace 12")
+                            yad --form --title="Settings" \
+                                --text="Choose an option:" \
+                                --field="Change Fonts:CHK" \
+                                --field="Change Colors:CHK" \
+                                --field="Placeholder:CHK" \
+                                --font="$selected_font" \
+                                --button="OK:0" --button="Cancel:1"
+
+
+                            # Show a color selection dialog and change the colors in the yad window
+                            selected_colors=$(yad --color-selection \
+                                --title="Select Colors")
+                            font_color=$(echo "$selected_colors" | awk -F':' '{print $1}')
+                            window_color=$(echo "$selected_colors" | awk -F':' '{print $2}')
+                            background_color=$(echo "$selected_colors" | awk -F':' '{print $3}')
+                            yad --form --title="Settings" \
+                                --text="Choose an option:" \
+                                --field="Change Fonts:CHK" \
+                                --field="Change Colors:CHK" \
+                                --field="Placeholder:CHK" \
+                                --fontcolor="$font_color" \
+                                --backcolor="$background_color" \
+                                --forecolor="$window_color" \
+                                --button="OK:0" --button="Cancel:1"
+                        elif [ "$placeholder" = "TRUE" ]; then
+                            # Placeholder for future feature
+                            yad --title="Settings" \
+                                --text="This feature is not yet available."
+                        else
+                            # Show an error message if no option is selected
+                            yad --title="Settings"
 --text="Please select an option."
 fi
 fi
@@ -88,39 +86,42 @@ yad --title="Error"
 --text="Invalid command: $user_command"
 ;;
 esac
-# Prompt the user to enter a new command
 
-yad --title="Terminal"
---text="Enter a command:"
---entry-text=""
---entry
---width="500"
---height="100"
---button="Execute:0" --button="Cancel:1" --default-button=0
-# Store the user's command
+# Prompt the user to enter a command
+user_command=$(yad --title="Terminal" \
+    --text="Enter a command:" \
+    --entry-text="" \
+    --entry \
+    --width="500" \
+    --height="100" \
+    --button="Execute:0" \
+    --button="Cancel:1" \
+    --default-button=0)
 
-user_command=$?
-# If the user did not click the "Cancel" button, execute the command
+# If the user clicked the "Execute" button, process the command
+if [ "$user_command" ]; then
+    case "$user_command" in
+        update)
+            exec xterm -e "sudo pacman -Syyu"
+            ;;
+        firefox)
+            exec firefox
+            ;;
+        settings)
+            # Show a dialog to change the settings
+            selected_options=$(yad --form \
+                --title="Settings" \
+                --text="Choose an option:" \
+                --field="Change Fonts:CHK" \
+                --field="Change Colors:CHK" \
+                --field="Placeholder:CHK")
 
-if [ $user_command -eq 0 ]; then
-# Execute the command
-case "$user_command_text" in
-update)
-exec xterm -e "sudo pacman -Syyu"
-;;
-firefox)
-exec firefox
-;;
-settings)
-# If the user types "settings" again, do nothing
-;;
-*)
-yad --title="Error"
---text="Invalid command: $user_command_text"
-;;
-esac
-fi
-# Show a color selection dialog and change the colors in the yad window
+            font=$(echo "$selected_options" | cut -d '|' -f 1)
+            color=$(echo "$selected_options" | cut -d '|' -f 2)
+            placeholder=$(echo "$selected_options" | cut -d '|' -f 3)
+
+            if [ "$color" = "TRUE" ]; then
+                # Show a color selection dialog and change the colors in the yad window
                 selected_colors=$(yad --color-selection \
                     --title="Select Colors")
                 font_color=$(echo "$selected_colors" | awk -F':' '{print $1}')
@@ -135,53 +136,21 @@ fi
                     --backcolor="$background_color" \
                     --forecolor="$window_color" \
                     --button="OK:0" --button="Cancel:1"
-elif [ "$placeholder" = "TRUE" ]; then
-# Placeholder for future feature
-yad --title="Settings"
---text="This feature is not yet available."
-else
-# Show an error message if no option is selected
-yad --title="Settings"
---text="Please select an option."
-fi
-fi
-;;
-*)
-yad --title="Error"
---text="Invalid command: $user_command"
-;;
-esac
-# Prompt the user to enter a new command
-
-yad --title="Terminal"
---text="Enter a command:"
---entry-text=""
---entry
---width="500"
---height="100"
---button="Execute:0" --button="Cancel:1" --default-button=0
-# Store the user's command
-
-user_command=$?
-# If the user did not click the "Cancel" button, execute the command
-
-if [ $user_command -eq 0 ]; then
-# Execute the command
-case "$user_command_text" in
-update)
-exec xterm -e "sudo pacman -Syyu"
-;;
-firefox)
-exec firefox
-;;
-settings)
-# If the user types "settings" again, do nothing
-;;
-*)
-yad --title="Error"
---text="Invalid command: $user_command_text"
-;;
-esac
+            elif [ "$placeholder" = "TRUE" ]; then
+                # Placeholder for future feature
+                yad --title="Settings" \
+                    --text="This feature is not yet available."
+            else
+                # Show an error message if no option is selected
+                yad --title="Settings" \
+                    --text="Please select an option."
+            fi
+            ;;
+        *)
+            yad --title="Error" \
+                --text="Invalid command: $user_command"
+            ;;
+    esac
 fi
 
- History button
+history button next?
