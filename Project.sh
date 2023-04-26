b284ff790099949a032044ce0e86f89411713c85
Drunkenguy <107703518+Drunkenguy@users.noreply.github.com>
2023-04-26 11:46:54 +0200

    Update Project.sh

--- Project.sh
+++ Project.sh
@@ -18,73 +18,107 @@ else
         user_command=$(echo "$command" | awk -F':' '{print $NF}')
 
         # Check the command entered by the user and execute the appropriate action
-        case "$user_command" in
-            update)
-                exec xterm -e "sudo pacman -Syyu"
-                ;;
-            firefox)
-                exec firefox
-                ;;
-            settings)
-                while true; do
-                    # Show a form with three options: Change Fonts, Change Colors, and Placeholder
-                    settings=$(yad --form --title="Settings" \
-                        --text="Choose an option:" \
-                        --field="Change Fonts:CHK" \
-                        --field="Change Colors:CHK" \
-                        --field="Placeholder:CHK" \
-                        --button="OK:0" --button="Cancel:1")
-                        
-                        
+case "$user_command" in
+    update)
+        exec xterm -e "sudo pacman -Syyu"
+        ;;
+    firefox)
+        exec firefox
+        ;;
+    settings)
+        # Show a dialog to change the settings
+        selected_options=$(yad --form \
+            --title="Settings" \
+            --text="Choose an option:" \
+            --field="Change Fonts:CHK" \
+            --field="Change Colors:CHK" \
+            --field="Placeholder:CHK")
 
-                    # Check which option the user selected and execute the appropriate action
-                    if [ $? -eq 0 ]; then
-                        # Extract settings from input string
-                        IFS=':' read -r fonts colors placeholder <<< "$settings"
-                        if [ "$fonts" = "TRUE" ]; then
-                            # Show a dialog to select a font and change the font in the yad window
-                            selected_font=$(yad --font \
-                                --title="Select a Font" \
-                                --fontname="Monospace 12")
-                            yad --form --title="Settings" \
-                                --text="Choose an option:" \
-                                --field="Change Fonts:CHK" \
-                                --field="Change Colors:CHK" \
-                                --field="Placeholder:CHK" \
-                                --font="$selected_font" \
-                                --button="OK:0" --button="Cancel:1"
+        font=$(echo "$selected_options" | cut -d '|' -f 1)
+        color=$(echo "$selected_options" | cut -d '|' -f 2)
+        placeholder=$(echo "$selected_options" | cut -d '|' -f 3)
 
+        if [ "$color" = "TRUE" ]; then
+            # Show a color selection dialog and change the colors in the yad window
+            selected_colors=$(yad --color-selection \
+                --title="Select Colors")
+            font_color=$(echo "$selected_colors" | awk -F':' '{print $1}')
+            window_color=$(echo "$selected_colors" | awk -F':' '{print $2}')
+            background_color=$(echo "$selected_colors" | awk -F':' '{print $3}')
+            yad --form --title="Settings" \
+                --text="Choose an option:" \
+                --field="Change Fonts:CHK" \
+                --field="Change Colors:CHK" \
+                --field="Placeholder:CHK" \
+                --fontcolor="$font_color" \
+                --backcolor="$background_color" \
+                --forecolor="$window_color" \
+                --button="OK:0" --button="Cancel:1"
+        elif [ "$placeholder" = "TRUE" ]; then
+            # Placeholder for future feature
+            yad --title="Settings" \
+                --text="This feature is not yet available."
+        else
+            # Show an error message if no option is selected
+            yad --title="Settings" \
+                --text="Please select an option."
+        fi
+        ;;
+    multiple)
+        # Execute multiple commands at once
+        exec xterm -e "sudo pacman -Syyu; firefox; ls -la"
+        ;;
+    appearance)
+        # Customize the appearance of the terminal emulator
+        selected_options=$(yad --form \
+            --title="Appearance" \
+            --text="Choose an option:" \
+            --field="Change Font:CHK" \
+            --field="Change Colors:CHK")
 
-                            # Show a color selection dialog and change the colors in the yad window
-                            selected_colors=$(yad --color-selection \
-                                --title="Select Colors")
-                            font_color=$(echo "$selected_colors" | awk -F':' '{print $1}')
-                            window_color=$(echo "$selected_colors" | awk -F':' '{print $2}')
-                            background_color=$(echo "$selected_colors" | awk -F':' '{print $3}')
-                            yad --form --title="Settings" \
-                                --text="Choose an option:" \
-                                --field="Change Fonts:CHK" \
-                                --field="Change Colors:CHK" \
-                                --field="Placeholder:CHK" \
-                                --fontcolor="$font_color" \
-                                --backcolor="$background_color" \
-                                --forecolor="$window_color" \
-                                --button="OK:0" --button="Cancel:1"
-                        elif [ "$placeholder" = "TRUE" ]; then
-                            # Placeholder for future feature
-                            yad --title="Settings" \
-                                --text="This feature is not yet available."
-                        else
-                            # Show an error message if no option is selected
-                            yad --title="Settings"
---text="Please select an option."
-fi
-fi
-;;
-*)
-yad --title="Error"
---text="Invalid command: $user_command"
-;;
+        font=$(echo "$selected_options" | cut -d '|' -f 1)
+        color=$(echo "$selected_options" | cut -d '|' -f 2)
+
+        if [ "$font" = "TRUE" ]; then
+            # Show a font selection dialog and change the font in the yad window
+            selected_font=$(yad --font \
+                --title="Select a Font" \
+                --fontname="Monospace 12")
+            yad --form --title="Appearance" \
+                --text="Choose an option:" \
+                --field="Change Font:CHK" \
+                --field="Change Colors:CHK" \
+                --font="$selected_font" \
+                --button="OK:0" --button="Cancel:1"
+        elif [ "$color" = "TRUE" ]; then
+            # Show a color selection dialog and change the colors in the yad window
+            selected_colors=$(yad --color-selection \
+                --title="Select Colors")
+            font_color=$(echo "$selected_colors" | awk -F':' '{print $1}')
+            window_color=$(echo "$selected_colors" | awk -F':' '{print $2}')
+            background_color=$(echo "$selected_colors" | awk -F':' '{print $3}')
+            yad --form --title="Appearance" \
+                --text="Choose an option:" \
+                --field="Change Font:CHK" \
+                --field="Change Colors:CHK" \
+                --fontcolor="$font_color" \
+                --backcolor="$background_color" \
+                --forecolor="$window_color" \
+                --button="OK:0" --button="Cancel:1"
+        else
+            # Show an error message if no option is selected
+            yad --title="Appearance" \
+                --text="Please select an option."
+        fi
+        ;;
+    privilege)
+        # Execute commands with elevated privileges
+        exec sudo "$user_command"
+        ;;
+    *)
+        yad --title="Error" \
+            --text="Invalid command: $user_command"
+        ;;
 esac
 
 # Prompt the user to enter a command
